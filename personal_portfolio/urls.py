d86abc029d55ac24e12333a7b38d3398d7832278
TareqHusein6 <husein.tareq@gmail.com>
2020-10-19 16:17:46 +0300

    adding images to project

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#used for adding a static url
+from django.conf.urls.static import static
+#get information from our settings file
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+#add static url for media
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
